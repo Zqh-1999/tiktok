e157ff4c9aa238290ead8f7336486b2103b3cb6e
ZhangYu <1257068932@qq.com>
2019-11-18 10:18:50 +0800

    video

--- tiktok_zy/src/views/Home.vue
+++ tiktok_zy/src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="home">
+    <van-swipe style="height: 100%;" vertical :show-indicators="false">
+      <van-swipe-item>
+        <!-- <video  autoplay controls name="muted" >
+          <source
+            src="http://video.jishiyoo.com/f843c93d4f3f4bee844690fe1fdfc750/28e2548c47ce4d0cb310697f0ca4c7a3-14818dd00765a9a86871da6734b1be28-ld.mp4"
+            type="video/mp4"
+          />
+        </video> -->
+        <iframe width="100%" height="100%" allow = "autoplay" src="http://video.jishiyoo.com/f843c93d4f3f4bee844690fe1fdfc750/28e2548c47ce4d0cb310697f0ca4c7a3-14818dd00765a9a86871da6734b1be28-ld.mp4" frameborder="0"></iframe>
+      </van-swipe-item>
+    </van-swipe>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+<style scoped>
+#home {
+  height: 100%;
 }
-</script>
+</style>
