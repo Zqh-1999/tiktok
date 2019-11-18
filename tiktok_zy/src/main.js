1d16d83e6584a9908d214b4e5d50ac6a25bd62b3
ZhangYu <1257068932@qq.com>
2019-11-18 08:48:23 +0800

    test

---
+++ tiktok_zy/src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import Vant from 'vant'
+import Axios from 'axios'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
+
+Vue.prototype.$Http = Axios
+
+Axios.defaults.baseURL = 'http://127.0.0.1'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
