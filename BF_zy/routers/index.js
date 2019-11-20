1af1cd8cc55f82f648c77ce64089c676e98a04b8
lixiaoyu <lixiaoyu@qq.com>
2019-11-20 09:37:41 +0800

    as

--- BF_zy/routers/index.js
+++ BF_zy/routers/index.js
@@ -4,6 +4,14 @@ const router = express.Router()
 
 const controller = require('../controller')
 
+// 视频
+// 查询所有视频
 router.get('/index', controller.index)
+
+// 条件查询视频
+router.get("/videos",controller.videos)
+
+// 根据id查询视频
+router.get("/videos/:id(\\d+)",controller.video)
 
 module.exports = router
